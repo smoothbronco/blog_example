ad44c301bffa7d29d3af83d254baa54d549a4f9c
smoothbronco <884takahiro63@gmail.com>
2022-07-14 22:13:59 +0900

    resolverの処理実装

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,35 +5,75 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/smoothbronco/blog_example/article/pb"
 	"github.com/smoothbronco/blog_example/graph/generated"
 	"github.com/smoothbronco/blog_example/graph/model"
 )
 
 // CreateArticle is the resolver for the createArticle field.
 func (r *mutationResolver) CreateArticle(ctx context.Context, input model.CreateInput) (*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	article, err := r.ArticleClient.CreateArticle(
+		ctx,
+		&pb.ArticleInput{
+			Author:  input.Author,
+			Title:   input.Title,
+			Content: input.Content,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return article, nil
 }
 
 // UpdateArticle is the resolver for the updateArticle field.
 func (r *mutationResolver) UpdateArticle(ctx context.Context, input model.UpdateInput) (*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	article, err := r.ArticleClient.UpdateArticle(
+		ctx,
+		int64(input.ID),
+		&pb.ArticleInput{
+			Author:  input.Author,
+			Title:   input.Title,
+			Content: input.Title,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return article, nil
 }
 
 // DeleteArticle is the resolver for the deleteArticle field.
 func (r *mutationResolver) DeleteArticle(ctx context.Context, input int) (int, error) {
-	panic(fmt.Errorf("not implemented"))
+	id, err := r.ArticleClient.DeleteArticle(ctx, int64(input))
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // Article is the resolver for the article field.
 func (r *queryResolver) Article(ctx context.Context, input int) (*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	article, err := r.ArticleClient.ReadArticle(ctx, int64(input))
+	if err != nil {
+		return nil, err
+	}
+
+	return article, nil
 }
 
 // Articles is the resolver for the articles field.
 func (r *queryResolver) Articles(ctx context.Context) ([]*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	articles, err := r.ArticleClient.ListArticle(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
