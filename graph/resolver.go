8f93c9deab24f68377d52d825ab3ec2185c57042
smoothbronco <884takahiro63@gmail.com>
2022-07-14 21:56:22 +0900

    resolverにgRPC client実装

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/smoothbronco/blog_example/article/client"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	ArticleClient *client.Client
+}
